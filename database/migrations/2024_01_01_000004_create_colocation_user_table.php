c7e1bba9b36c75a89e075b3417cab0a84824b91c
zakariadachi <dachiziko@gmail.com>
2026-02-24 12:21:03 +0000

    ajouter membership migration

---
+++ database/migrations/2024_01_01_000004_create_colocation_user_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('colocation_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->enum('role', ['owner', 'member']);
+            $table->timestamp('joined_at');
+            $table->timestamp('left_at')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('colocation_user');
+    }
+};
