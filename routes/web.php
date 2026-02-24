8e36a3e471f0fc15b32de2683b12556da3eaea8d
zakariadachi <dachiziko@gmail.com>
2026-02-24 14:27:33 +0000

    ajouter le route de profile dans web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,9 @@ Route::get('/', function () {
 });
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
