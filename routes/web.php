ae4aebd5d2fa16b39ba496c85e28a444a18caaa0
zakariadachi <dachiziko@gmail.com>
2026-02-26 05:11:14 +0000

    Update routes with welcome and dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
 use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/dashboard', function () {
+    return redirect()->route('colocations.index');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/invitations/{token}', [InvitationController::class, 'show'])->name('invitations.show');
+Route::post('/invitations/{token}/accept', [InvitationController::class, 'accept'])->middleware('auth')->name('invitations.accept');
 
 Route::middleware(['auth', 'banned'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -14,6 +22,9 @@ Route::middleware(['auth', 'banned'])->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
     Route::resource('colocations', ColocationController::class);
+    Route::post('/colocations/{colocation}/invite', [InvitationController::class, 'send'])->name('colocations.invite');
+    Route::post('/colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('colocations.leave');
+    Route::delete('/colocations/{colocation}/members/{member}', [ColocationController::class, 'removeMember'])->name('colocations.removeMember');
 });
 
 require __DIR__.'/auth.php';
