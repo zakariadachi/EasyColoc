69af382ead762993ba3363dd1370bfa9c17e4923
zakariadachi <dachiziko@gmail.com>
2026-02-26 14:15:01 +0000

    fix: update redirect after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/profile');
+        return redirect()->intended('/dashboard');
     }
 
     /**
