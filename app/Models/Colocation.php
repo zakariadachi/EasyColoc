f4037859ddce8c429886336116a41190c1f2d631
zakariadachi <dachiziko@gmail.com>
2026-02-27 04:26:17 +0000

    feat: add expenses relationship to Colocation model

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Colocation extends Model
 {
@@ -21,5 +22,10 @@ class Colocation extends Model
     {
         return $this->belongsToMany(User::class, 'colocation_user')
             ->withPivot('role', 'joined_at', 'left_at');
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
     }
 }
