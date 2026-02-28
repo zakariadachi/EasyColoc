2b2e1a0e209ae7d5eaaeeb591c37a5e4440aa76f
zakariadachi <dachiziko@gmail.com>
2026-02-28 03:05:39 +0000

    feat: register IsAdmin middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'banned' => \App\Http\Middleware\CheckBanned::class,
+            'admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
