82db20da8ee413bcdbff764a3a9028f0193c5d61
zakariadachi <dachiziko@gmail.com>
2026-02-28 03:05:59 +0000

    feat: add admin routes with middleware protection

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SettlementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,6 +32,17 @@ Route::middleware(['auth', 'banned'])->group(function () {
     Route::post('/colocations/{colocation}/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
     Route::put('/colocations/{colocation}/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
     Route::delete('/colocations/{colocation}/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+    
+    Route::get('/colocations/{colocation}/settlements', [SettlementController::class, 'index'])->name('settlements.index');
+    Route::post('/colocations/{colocation}/settlements', [SettlementController::class, 'store'])->name('settlements.store');
+    Route::post('/colocations/{colocation}/settlements/{settlement}/paid', [SettlementController::class, 'markAsPaid'])->name('settlements.markAsPaid');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('index');
+    Route::get('/users', [\App\Http\Controllers\AdminController::class, 'users'])->name('users');
+    Route::post('/users/{user}/ban', [\App\Http\Controllers\AdminController::class, 'banUser'])->name('users.ban');
+    Route::post('/users/{user}/unban', [\App\Http\Controllers\AdminController::class, 'unbanUser'])->name('users.unban');
 });
 
 require __DIR__.'/auth.php';
